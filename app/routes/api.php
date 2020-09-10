ca0cc07d0ad6f279d7c849332e9fff58c2c31e5c
a.gorbunov <a.gorbunov@id-east.ru>
2020-09-10 14:07:50 +0000

    describe routes for api

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth.guest:api')
+    ->group(function () {
+        Route::resource('message', MessageController::class)
+            ->only(['store', 'index']);
+    });

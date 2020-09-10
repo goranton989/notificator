13c691e6575402118f7d534550e58514803ba5c0
a.gorbunov <a.gorbunov@id-east.ru>
2020-09-10 14:05:35 +0000

    remove uselles docker configuration files

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-HORIZON_LOGS = /srv/horizon.log
-
 start_supervisor() {
   service supervisor stop
   service supervisor start
@@ -11,14 +9,9 @@ start_supervisor() {
   supervisorctl update
 }
 
-clear_horizon_logs() {
-  echo "" > ${HORIZON_LOGS}
-}
-
 if [ "$1" == "horizon" ]; then
   echo "Run as horizon"
   start_supervisor
-  clear_horizon_logs
 fi
 
 shift
